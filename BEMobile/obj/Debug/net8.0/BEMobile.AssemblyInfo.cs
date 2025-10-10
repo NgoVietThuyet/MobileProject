c2881cab69c1f2f2f5fe862a8248b0f6899a6303
ducvanz <ducvan2745@gmail.com>
2025-10-10 17:00:08 +0700

    merge with van

--- BEMobile/obj/Debug/net8.0/BEMobile.AssemblyInfo.cs
+++ BEMobile/obj/Debug/net8.0/BEMobile.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BEMobile")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ab2bec28d3514b7813355c31a2c9825d7ed53da7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c3d947df45ffa2f206faeacb38e3edfcfa95b8bf")]
 [assembly: System.Reflection.AssemblyProductAttribute("BEMobile")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BEMobile")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
